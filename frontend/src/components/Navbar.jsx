d590c12f983f7b1f15393e2297fe7c85fa340743
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-05-18 01:39:33 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link,Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useContext } from "react";
 import { AuthContext } from '../context/AuthContext';
 import { useAuthStatus } from '../hooks/useAuthStatus';
